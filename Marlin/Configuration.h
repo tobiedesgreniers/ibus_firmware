65a1422188870320fa1a687e4b960327aaeceb0e
tobiedesgreniers <tobiedesgreniers300@gmail.com>
2021-11-14 14:56:05 -0500

    endstop safety + bed size

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1359,8 +1359,8 @@
 // @section machine
 
 // The size of the printable area
-#define X_BED_SIZE 200
-#define Y_BED_SIZE 200
+#define X_BED_SIZE 377
+#define Y_BED_SIZE 370
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
